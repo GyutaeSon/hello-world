c2f687ac05edd36916c5dcb46dde802cecb6f7c6
GyutaeSon <zw19120n@otemae.ac.jp>
2021-02-25 16:44:10 +0900

    NLP05-修正(スライス)

--- noc_05.py
+++ noc_05.py
@@ -6,37 +6,17 @@
 
 sentence = "I am an NLPer"
 
-n_value = 2
-
 #n-gramを作る関数
-def ngram(n_value, bunsyo):
-    waketa = bunsyo
-    #文字列か判断
-    if(type(waketa) == str) :
-        waketa = waketa.replace(",", "")
-        waketa = waketa.replace(".", "")
-        waketa = waketa.replace(";", "")
-        waketa = waketa.replace(" ", "")
-        print(waketa)
-    #文字列やリストの長さ
-    check = len(waketa)
-
+def ngram(n, waketa):
     res= []
-    num = 0
+    for i in range(len(waketa)) :
+        if i+n > len(waketa):
+            return res
+        res.append(waketa[i : i+n])
 
-    for i in range(check) :
-        res.append([])
-        num = i
-        for s in range(n_value) :
-            if i+s >= check:
-                res[i].append(waketa[i - num])
-                num = num - 1
-            else :
-                res[i].append(waketa[num+s])
-    return res
 
 print("単語bi-gram")
-print(ngram(n_value, sentence.split()))
+print(ngram(2, sentence.split()))
 
 print("文字bi-gram")
-print(ngram(n_value, sentence))
+print(ngram(2, sentence))
