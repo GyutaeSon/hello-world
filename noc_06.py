194988e9866efa9d855a6cdb1c003f7729ab52e4
GyutaeSon <zw19120n@otemae.ac.jp>
2021-02-25 17:30:41 +0900

    NLP06

--- noc_06.py
+++ noc_06.py
@@ -7,3 +7,26 @@
 a = "paraparaparadise"
 b = "paragraph"
 
+def ngram(n, bun) :
+    res = []
+    for it in range(len(bun)) :
+        if it + n > len(bun) :
+            return res
+        res.append(bun[it : it + n])
+
+X = set(ngram(2, a))
+Y = set(ngram(2, b))
+
+print("X:", X)
+print("Y:", Y)
+
+wa = X | Y
+seki = X & Y
+sa = X - Y
+
+print("和集合:", wa)
+print("積集合", seki)
+print("差集合", sa)
+
+qest="’se’というbi-gramがXおよびYに含まれるか:{0}".format('se' in wa)
+print(qest)
